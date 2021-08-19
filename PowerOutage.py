4820a6380152e1ee4cb1305c3ff97ed3216d0839
Mohammd Reza Mollasalehi <49244782+mm3906078@users.noreply.github.com>
2021-08-19 10:57:34 +0430

    add send_mail

--- PowerOutage.py
+++ PowerOutage.py
@@ -5,13 +5,18 @@ import ntplib
 import time
 import telegram
 import pytz
+import smtplib
 from datetime import datetime
+
+# change telegram token & chat_id in notify_tel
+# change send_mail & rciver_mail & pass in send_mail
+# change hostname in main
 
 tz_iran = pytz.timezone('Iran')
 count = 0
 triger = False
 
-def notify_ending(message):
+def notify_tel(message):
     token = 'Telegram_token'
     chat_id = 'Telegram_chat_id'
     bot = telegram.Bot(token=token)
@@ -36,6 +41,26 @@ def check_ping(hostname):
         print("Network Error")
         return 2
 
+def send_mail(body):
+    send_mail = "sender_mail"
+    recive_mail = "rciver_mail"
+    passwd = "mail_pass"
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    # stable connection
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(send_mail , passwd)
+    subject = 'PowerOutage'
+    msg = f"Subject : {subject} \n\n {body}"
+    server.sendmail(
+        send_mail,
+        recive_mail,
+        msg
+    )
+    print("EMAIL SENT!!")
+    server.quit()
+
 def main(alarm_triger):
     hostname = "192.168.1.20"
 
@@ -46,11 +71,11 @@ def main(alarm_triger):
 
     print(f"\n\n................{current_time_hours}:{current_time_min}....................")
 
-    if (check_ping(hostname) == 0):
+    if (check_ping(hostname) != 0):
         print("ITS UP")
         return 0
 
-    elif (check_ping(hostname) != 0):
+    elif (check_ping(hostname) == 0):
         if (int(current_time_hours) <= 12 and current_time == "AM"):
             print("EXPECTED DOWN TIME")
             return 0
@@ -59,24 +84,26 @@ def main(alarm_triger):
             return 0
         else:
             if (alarm_triger == False):
-                notify_ending("UNEXPECTED DOWN TIME Triger Alarm")
+                notify_tel("UNEXPECTED DOWN TIME Triger Alarm")
+                # send_mail("UNEXPECTED DOWN TIME Triger Alarm")
                 return 2
             elif (alarm_triger == True):
                 print("UNEXPECTED DOWN TIME")
                 return 3
-
-
+                
 while (True):
     r = main(triger)
     if(r == 2):
         triger = True
-
+    else:
+        triger = False
+        count  = 0
     if (triger == True):
         count += 1
-        time.sleep(60)
-        if (count == 360):
+        if (count == 10):
+            send_mail("UNEXPECTED DOWN TIME Triger Alarm")
             count = 0
             triger = False
-
+        time.sleep(60)
     else:
         time.sleep(60)
